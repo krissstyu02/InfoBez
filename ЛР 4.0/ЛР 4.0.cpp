98610e0cdd56af36f34b85c524fc7354a1d1ae04
Кристина <kristi090217@gmail.com>
2022-10-08 10:29:40 +0300

    ЛР 4.3

---
+++ ЛР 4.0/ЛР 4.0.cpp
@@ -0,0 +1,110 @@
+﻿// ЛР 4.0.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <fstream>
+using namespace std;
+
+int n; // размер алфавита
+int k; // длина слова
+vector<char> CompObj; //комб.объект
+vector<char> alphabet; // алфавит
+
+string result = "";
+
+void InputAlpha() // Ввод алфавита
+{
+	n = 3;
+	k = 8;
+	alphabet.resize(n);
+	alphabet = { '2','5','7' };
+}
+
+bool Condition(string inp)
+{
+	if (count(inp.begin(), inp.end(), '2') == 2 && count(inp.begin(), inp.end(), '5') == 2)
+		return true;
+	else
+		return false;
+	return true;
+}
+
+void PrintCompObjWithCondition(int k)
+{
+	string ObjToPrint = "";
+	for (int i = 0; i < k; i++)
+	{
+		ObjToPrint += CompObj[i];
+	}
+	if (Condition(ObjToPrint))
+		result += ObjToPrint + "\n";
+
+	/*cout << ObjToPrint << endl;*/
+}
+
+bool HasNextCompObj()
+{
+	int kol = 0;
+	for (int i = 0; i < k; i++)
+		if (CompObj[i] == alphabet[n - 1]) kol++;
+	return (kol != k);
+}
+void NextCompObj()
+{
+	for (int i = k - 1; i > -1; i--)
+	{
+		if (CompObj[i] == alphabet[n - 1])
+		{
+			CompObj[i] = alphabet[0];
+			continue;
+		}
+		CompObj[i]++;
+
+		while (!(find(alphabet.begin(), alphabet.end(), CompObj[i]) != alphabet.end()))
+		{
+			CompObj[i]++;
+		}
+		break;
+	}
+}
+
+void Process()
+{
+	CompObj.resize(k);
+	for (int i = 0; i < k; i++)
+		CompObj[i] = alphabet[0];
+	while (HasNextCompObj())
+	{
+		PrintCompObjWithCondition(k);
+		NextCompObj();
+	}
+
+	PrintCompObjWithCondition(k);
+}
+
+void main()
+{
+	setlocale(LC_ALL, "RUS");
+	InputAlpha();
+	Process();
+	ofstream output("output.txt");
+	
+	if (output.is_open())
+	{
+		output << result << endl;
+	}
+
+}
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.

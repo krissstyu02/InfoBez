f455a00ca5321aa63d7c189a2f01c99282a84bad
Кристина <kristi090217@gmail.com>
2022-11-24 16:07:59 +0300

    ЛР 9.2

---
+++ ЛР 9.2/ЛР 9.2.cpp
@@ -0,0 +1,154 @@
+﻿// ЛР 9.2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <math.h>
+#include <tuple>
+#include <vector>
+#include <stdio.h>
+#include <algorithm>
+#include <queue>
+#include <string>
+#include <iostream>
+#include <fstream>
+using namespace std;
+typedef long long ll;
+
+string getRandomAccess() {
+	static vector<string> tmp = { "o", "i", "w", "r", "t", "g" };
+	return tmp[rand() % 6];
+}
+
+vector<vector<string>> generateGraph() {
+	int n = 5;
+	vector<vector<string>> r = vector<vector<string>>(n, vector<string>(n, " "));
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < n; j++)
+			if (i != j)
+				r[i][j] = getRandomAccess();
+
+	return r;
+}
+
+vector<vector<string>> badExsample() {
+	int n = 5;
+	vector<vector<string>> r = vector<vector<string>>(n, vector<string>(n, " "));
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < n; j++)
+			if (i != j)
+				if (i == 0 || j == 0) {
+					r[i][j] = "r";
+				}
+				else {
+					r[i][j] = getRandomAccess();
+				}
+
+
+	return r;
+}
+
+bool contains(string s, char c) {
+	for (auto i : s)
+		if (i == c)
+			return true;
+	return false;
+}
+
+bool isHaveTgWay(vector<vector<string>>& matr, int x, int y) {
+	vector<bool> visited(matr.size(), false);
+	queue<int> q;
+	q.push(x); visited[x] = true;
+
+	while (!q.empty()) {
+		int v = q.front(); q.pop();
+
+		if (v == y) {
+			return true;
+		}
+
+		for (int j = 0; j < matr.size(); j++) {
+			if (!visited[j]) {
+				if (contains(matr[v][j], 't') || contains(matr[v][j], 'g') || contains(matr[j][v], 't') || contains(matr[j][v], 'g')) {
+					q.push(j);
+					visited[j] = true;
+				}
+			}
+		}
+	}
+	return false;
+}
+
+bool possibleAccess(vector<vector<string>>& matr, string a, int x, int y) {
+	vector<char> rules;
+	for (auto i : a)
+		rules.push_back(i);
+
+	//Первый критерий
+	vector<vector<int>> neededRules(rules.size(), vector<int>());
+	for (int r = 0; r < rules.size(); r++) {  // для каждого правила
+
+		for (int i = 0; i < matr.size(); i++) {
+			for (int j = 0; j < matr.size(); j++) {
+				if (contains(matr[i][j], rules[r])) { // проверяем, существует ли оно в матрице и запоминаем, кто им обладает
+					neededRules[r].push_back(i);
+					break;
+				}
+			}
+		}
+	}
+	for (auto i : neededRules) {
+		if (i.empty())
+			return false;
+	}
+
+	// второй критерий
+	vector<bool> canReachRules(rules.size());
+	for (int r = 0; r < rules.size(); r++) { // для каждого правила
+		bool f = false;
+
+		for (int k = 0; k < neededRules[r].size(); k++) {
+			if (isHaveTgWay(matr, x, neededRules[r][k])) { // ищем наличие tg пути хотя бы до одного обладателя этого правила
+				f = true;
+				break;
+			}
+		}
+
+		if (!f) {
+			return false;
+		}
+	}
+
+	return true;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	srand(time(0));
+
+	//vector<vector<string>> matr = generateGraph();
+	vector<vector<string>> matr = badExsample();
+
+	for (auto i : matr) {
+		for (auto j : i)
+			cout << j << ' ';
+		cout << endl;
+	}
+
+	while (true) {
+		string tmpStr;
+		int i, j;
+		cin >> tmpStr >> i >> j;
+		cout << possibleAccess(matr, tmpStr, i, j) << endl;
+	}
+}
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
